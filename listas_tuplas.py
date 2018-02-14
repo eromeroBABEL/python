731ade64950366b79023395665399b34eefe295f
Eduardo <eduardo.romero@babel.es>
2018-02-14 16:48:17 +0100

    Añadido listas_tuplas

---
+++ listas_tuplas.py
@@ -0,0 +1,12 @@
+"""
+Listas y Tuplas
+TUPLA: ('ejemplo1',34,'Eduardo',true). Menos recursos en memoria. Inmutable
+LISTA: ['Eduardo',32,'Garrido']
+"""
+
+tupla=('uno',44,"prueba","final")
+print tupla[1]
+for x in tupla:
+	print x
+
+
