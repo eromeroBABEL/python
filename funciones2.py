f92bc42f4d024da15c8dbb235de798c208262574
Eduardo <eduardo.romero@babel.es>
2018-02-14 19:56:30 +0100

    dfads

---
+++ funciones2.py
@@ -0,0 +1,21 @@
+#-*- coding: utf-8 -*-
+#Utilizo las funciones
+import funciones
+
+funciones.imprimir()
+
+
+print funciones.funcion_holamundo()
+
+print funciones.parimpar(7)
+
+print " "
+
+
+
+
+
+#funciones.datos("04345435D","Eduardo","Romero","Garrido","Gómez")
+
+
+print funciones.iniciales("Eduardo","Romero","Garrido")
