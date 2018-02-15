3416241e9f5f13932868c33cf89713e53918f9b7
root <root@localhost.localdomain>
2018-02-15 18:21:58 +0100

    fdaf

--- librerias/ficheros.py
+++ librerias/ficheros.py
@@ -17,4 +17,13 @@ def crearDirectorio(path_file):
 	else:
 		print "Ya existe el directorio"
 		
+
+def VerVariablesEntorno():
+#Funcion que devuevla el usuario, la shell y el path con el que estamos trabajando	
+	for x,y in os.environ.iteritems():
+		if x=="USER" or x=="PATH" or x=="SHELL":
+			print x+"-->"+y
+	print os.environ.get('USER')
+	print os.environ.get('PATH')
+	print os.environ.get('SHELL')
 	
