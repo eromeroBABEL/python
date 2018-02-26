51bb82ed512c32dd300ac276646f9bb88f7aa28a
root <root@localhost.localdomain>
2018-02-26 16:40:01 +0100

    Añadido script de ssh

---
+++ ejer14_ssh.py
@@ -0,0 +1,15 @@
+import sys,paramiko
+
+host_address="192.168.1.59"
+
+if len(sys.argv)==2:
+	password="oracle"
+	command=sys.argv[1]
+	try:
+		client=paramiko.SSHClient()
+		client.load_system_host_keys()
+		client.connect(host_address,22,"root",password)
+		stdin,stdout,stderr= client.exec_command(command)
+		print stdout.read()
+	finally:
+		client.close()
